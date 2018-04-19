18ec1fd5dc5f984270e0e569c73d38ee15306b92
Taranvir <thidkyar@gmail.com>
2018-04-19 18:50:18 +0000

    Adds conditionals exercises

---
+++ IceCream.js
@@ -0,0 +1,8 @@
+// Do not modify these first two lines
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("Root Beer");
+console.log(iceCreamFlavours);
+console.log('Total flavours are: ' iceCreamFlavours + .length);
